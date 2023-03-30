ca10718fb56469fc2caa46ab39a87cb23a2d59cd
Uberts94 <s292522@studenti.polito.it>
2023-03-30 11:28:11 +0200

    to be finished

--- Lab6_frequentlyBought/src/main/java/it/polito/bigdata/spark/example/SparkDriver.java
+++ Lab6_frequentlyBought/src/main/java/it/polito/bigdata/spark/example/SparkDriver.java
@@ -1,6 +1,11 @@
 package it.polito.bigdata.spark.example;
 
 import org.apache.spark.api.java.*;
+
+import scala.Tuple2;
+
+import java.util.List;
+
 import org.apache.spark.SparkConf;
 	
 public class SparkDriver {
@@ -15,11 +20,11 @@ public class SparkDriver {
 
 	
 		// Create a configuration object and set the name of the application
-		SparkConf conf=new SparkConf().setAppName("Spark Lab #6");
+		SparkConf conf=new SparkConf().setAppName("Spark Lab #6").setMaster("local");
 		
 		// Use the following command to create the SparkConf object if you want to run
 		// your application inside Eclipse.
-		// SparkConf conf=new SparkConf().setAppName("Spark Lab #6").setMaster("local");
+		//SparkConf conf=new SparkConf().setAppName("Spark Lab #6").setMaster("local");
 		// Remember to remove .setMaster("local") before running your application on the cluster
 		
 		// Create a Spark Context object
@@ -27,12 +32,15 @@ public class SparkDriver {
 
 		
 		// Read the content of the input file
-		// JavaRDD<String> inputRDD = sc.textFile(inputPath);
+		JavaRDD<String> inputRDD = sc.textFile(inputPath);
 
-		// TODO .......
+		JavaPairRDD<String, String> userReviews= inputRDD.mapToPair(line -> {
+			String[] entry = line.split(",");
+			return new Tuple2<String, String>(entry[3], entry[2]);
+		});
 		
 		// Store the result in the output folder
-		// resultRDD.saveAsTextFile(outputPath);
+		inputRDD.saveAsTextFile(outputPath);
 
 		// Close the Spark context
 		sc.close();
